e9baea5629b85936479e74f82930e370d4b218b0
Piyush Ramavat <piyush.ramavat@gmail.com>
2024-10-29 16:50:41 +1100

    CRUD Result operations done

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { healthCheckHandler } from "./health-check";
 import { RestHelper, withErrorHandler } from "../lib/utils";
-import { getResultHandler, saveResultHandler } from "./results";
+import { getResultHandler, getResultListHandler, saveResultHandler, updateResultHandler } from "./results";
 
 const apiRouter = Router();
 
@@ -10,7 +10,9 @@ apiRouter.get("/health-check", healthCheckHandler);
 
 // API routes
 apiRouter.post("/api/results", withErrorHandler(saveResultHandler));
-apiRouter.get("/api/results", withErrorHandler(getResultHandler));
+apiRouter.get("/api/results/list", withErrorHandler(getResultListHandler));
+apiRouter.get("/api/results/:resultId", withErrorHandler(getResultHandler));
+apiRouter.put("/api/results/:resultId", withErrorHandler(updateResultHandler));
 
 // Unknown
 apiRouter.use((req, res) => {
--- server/src/services/result-service.ts
+++ server/src/services/result-service.ts
@@ -1,20 +1,55 @@
 import { SaveResultRequest, SaveResultRequestSchema } from "../lib/types";
 import { dbService } from "../lib/db-service";
+import { Result } from "@prisma/client";
 
-export const saveResult = (resultData: SaveResultRequest) => {
+export const saveResult = async (resultData: SaveResultRequest): Promise<Result> => {
   const parsed = SaveResultRequestSchema.safeParse(resultData);
   if (!parsed.success) {
     throw parsed.error;
   }
   const db = dbService();
-  return db.result.create({
+
+  return await db.result.create({
     data: {
       ...resultData,
     },
   });
 };
 
-export const getAllResults = () => {
+export const findAllResults = async (): Promise<Result[]> => {
   const db = dbService();
-  return db.result.findMany();
+
+  return await db.result.findMany();
+};
+
+export const findResult = async (resultId: number): Promise<Result | null> => {
+  const db = dbService();
+
+  return await db.result.findFirst({
+    where: {
+      id: resultId,
+    },
+  });
+};
+
+export const updateResult = async (resultId: number, resultData: SaveResultRequest): Promise<Result> => {
+  const db = dbService();
+
+  const parsed = SaveResultRequestSchema.safeParse(resultData);
+  if (!parsed.success) {
+    throw parsed.error;
+  }
+
+  const updated = await db.result.update({
+    data: { ...resultData },
+    where: { id: resultId },
+  });
+
+  return updated;
+};
+
+export const deleteAll = async () => {
+  const db = dbService();
+
+  return await db.result.deleteMany();
 };
